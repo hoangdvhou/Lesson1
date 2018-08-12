4444f0a86a30fc03cc52b887ffc01c60ec06928c
hoangdvhou <hoangdv.hou@gmail.com>
2018-08-12 23:05:40 +0700

    Lesson1 commit

---
+++ src/GameWindow.java
@@ -0,0 +1,63 @@
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class GameWindow extends JFrame {
+    GameCanvas gameCanvas;
+
+
+
+
+    public GameWindow() {
+        //Setup game window
+
+        this.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {
+                System.out.println("windowOpened");
+            }
+
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.out.println("windowClosed");
+
+            }
+
+            @Override
+            public void windowIconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeiconified(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowActivated(WindowEvent e) {
+
+            }
+
+            @Override
+            public void windowDeactivated(WindowEvent e) {
+
+            }
+            ;
+        });
+        this.setSize(600,800);
+        this.setResizable(false);
+        this.setTitle("Micro-war");
+
+        //Setup canvas
+        gameCanvas = new GameCanvas();
+        this.setContentPane(gameCanvas);
+
+
+        this.setVisible(true);
+    }
+}
